f0baff4abef896d0a4734115029406b1e81fd8a4
Luis <luiscornieles30@gmail.com>
2025-12-13 17:51:53 -0400

    SABADITO

--- volumen.py
+++ volumen.py
@@ -1,6 +1,8 @@
 from PyQt5.QtWidgets import QDialog, QPushButton, QVBoxLayout, QSlider
 from PyQt5.QtCore import Qt
 from pygame import mixer
+idioma_actual = "es"
+
 
 
 class Volumen(QDialog):
@@ -14,29 +16,72 @@ class Volumen(QDialog):
         self.slider.setMaximum(200)
         self.slider.setValue(int(mixer.music.get_volume() * 200))
 
-        boton_volver = QPushButton("Volver")
+        self.boton_volver = QPushButton()
 
         layout = QVBoxLayout()
         layout.addWidget(self.slider)
-        layout.addWidget(boton_volver)
+        layout.addWidget(self.boton_volver)
         self.setLayout(layout)
 
         self.slider.valueChanged.connect(self.cambiar_volumen)
 
-        boton_volver.clicked.connect(self.reject)
+        self.boton_volver.clicked.connect(self.reject)
+        self.actualizar_textos()
+        
 
     def cambiar_volumen(self, valor):
         mixer.music.set_volume(valor / 100)
+    def actualizar_textos(self):
+        if   idioma_actual == "es":
+            self.boton_volver.setText("Volver")
+        elif idioma_actual == "ru":
+            self.boton_volver.setText("Возвращаться")
+        elif idioma_actual == "ch":
+            self.boton_volver.setText("返回")
+        else:
+            self.boton_volver.setText("Back")
+
 class Idioma(QDialog):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Idioma")
         self.setFixedSize(300, 200)
 
+        boton_es = QPushButton("Español")
+        boton_en = QPushButton("English")
+        boton_ru = QPushButton("русский")
+        boton_ch = QPushButton("傳統中文")
         boton_volver = QPushButton("Volver")
 
         layout = QVBoxLayout()
+        layout.addWidget(boton_es)
+        layout.addWidget(boton_en)
+        layout.addWidget(boton_ru)
+        layout.addWidget(boton_ch)
         layout.addWidget(boton_volver)
         self.setLayout(layout)
 
+        boton_es.clicked.connect(self.espanol)
+        boton_en.clicked.connect(self.ingles)
+        boton_ru.clicked.connect(self.ruso)
+        boton_ch.clicked.connect(self.chino)
         boton_volver.clicked.connect(self.reject)
+
+    def espanol(self):
+        global idioma_actual
+        idioma_actual = "es"
+        self.reject()
+
+    def ingles(self):
+        global idioma_actual
+        idioma_actual = "en"
+        self.reject()
+    def ruso(self):
+        global idioma_actual
+        idioma_actual = "ru"
+        self.reject()
+    def chino(self):
+        global idioma_actual
+        idioma_actual = "ch"
+        self.reject()
+
--- xd.py
+++ xd.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import QDialog, QPushButton, QVBoxLayout
 from volumen import *
+import volumen
 
 class VentanaPausa(QDialog):
     def __init__(self):
@@ -7,24 +8,50 @@ class VentanaPausa(QDialog):
         self.setWindowTitle("Pausa")
         self.setFixedSize(300, 200)
 
-
-        boton_continuar = QPushButton("Continuar")
-        boton_idioma = QPushButton("Idioma")
-        boton_volumen = QPushButton("Volumen")
-        boton_salir = QPushButton("Salir")
+      
+        self.boton_continuar = QPushButton()
+        self.boton_idioma = QPushButton()
+        self.boton_volumen = QPushButton()
+        self.boton_salir = QPushButton()
 
         layout = QVBoxLayout()
-        layout.addWidget(boton_continuar)
-        layout.addWidget(boton_idioma)
-        layout.addWidget(boton_volumen)
-        layout.addWidget(boton_salir)
+        layout.addWidget(self.boton_continuar)
+        layout.addWidget(self.boton_idioma)
+        layout.addWidget(self.boton_volumen)
+        layout.addWidget(self.boton_salir)
         self.setLayout(layout)
 
+     
+        self.boton_continuar.clicked.connect(self.continuar)
+        self.boton_idioma.clicked.connect(self.abrir_idioma)
+        self.boton_volumen.clicked.connect(self.abrir_volumen)
+        self.boton_salir.clicked.connect(self.salir)
 
-        boton_continuar.clicked.connect(self.continuar)
-        boton_idioma.clicked.connect(self.abrir_idioma)   
-        boton_volumen.clicked.connect(self.abrir_volumen)
-        boton_salir.clicked.connect(self.salir)
+       
+        self.actualizar_textos()
+
+    def actualizar_textos(self):
+        if volumen.idioma_actual == "es":
+            self.boton_continuar.setText("Continuar")
+            self.boton_idioma.setText("Idioma")
+            self.boton_volumen.setText("Volumen")
+            self.boton_salir.setText("Salir")
+        elif volumen.idioma_actual == "ru":
+            self.boton_continuar.setText("Продолжать")
+            self.boton_idioma.setText("Язык")
+            self.boton_volumen.setText("Объем")
+            self.boton_salir.setText("Выходить")
+        elif volumen.idioma_actual == "ch":
+            self.boton_continuar.setText("繼續")
+            self.boton_idioma.setText("語言")
+            self.boton_volumen.setText("體積")
+            self.boton_salir.setText("出去")
+        else:
+            self.boton_continuar.setText("Continue")
+            self.boton_idioma.setText("Language")
+            self.boton_volumen.setText("Volume")
+            self.boton_salir.setText("Exit")
+
     def continuar(self):
         self.opcion = "continuar"
         self.accept()
@@ -35,9 +62,9 @@ class VentanaPausa(QDialog):
 
     def abrir_volumen(self):
         ventana = Volumen()
-        ventana.exec_()  
+        ventana.exec_()
 
     def abrir_idioma(self):
         ventana = Idioma()
-        ventana.exec_()   
-
+        ventana.exec_()
+        self.actualizar_textos() 
